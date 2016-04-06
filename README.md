68bd88616e06d99a839cf6f855380eaf4943bbe4
Gregory C <sgchris@gmail.com>
2016-04-07 00:26:16 +0300

    Refactored the main JS - normalization. Updated readme file

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-Create PHP scripts for testing purposes
+Intro
+=====
+The application allows easily to work on small PHP scripts.
+Usually these kind of scripts are used for development, testing or learning purposes.
+
+This application must not (!) be installed on production servers, or any other important servers
+that may be exposed to incoming connections
+
+Installation
 =======================================
+
+Navigate to a doc root
+```
+> cd /var/www/html
+```
+
+Clone the repo to a folder (e.g. ztest)
+```
+> git clone https://github.com/sgchris/PHPScriptsTester.git ztest
+```
+
+The app is performing I/O operations on files, therefore change permissions of the new folder
+```
+> chown :www-data ztest
+> chmod g+w ztest
+```
+
+That's it!. Navigate to the application in your browser
+http://localhost/ztest
+
+TODO
+====
+- [ ] Fix editor PHP highlighting
+- [ ] When creating new script, select it automatically
+- [ ] Create "Confirm" and "Prompt" dialogs
+- [ ] Initialize editor on the beginning, and once saving ask for a script name
+- [ ] Add icon to execute the script in another window, and load source by clicking the name itself
+
+Credits
+=======
+
+- [CodeMirror](http://codemirror.net) online editor, which is available on [GitHub](https://github.com/codemirror/codemirror)
+- [MaterializeCSS](http://materializecss.com/) - CSS framework for material design
+- [jQuery](https://jquery.com/) - jQuery JS framework
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 (function() {
 	
-	// current selected script name on the files list on the left
-	var selectedScriptName = null;
-	
 	var error = function(str) {
 		Materialize && Materialize.toast('ERROR! ' + str, 4000, 'red accent-2');
 	}
@@ -11,196 +8,217 @@
 		Materialize && Materialize.toast(str, 2000, 'teal');
 	};
 
-	var createNewScript = function(scrName) {
-		return $.ajax({
-			type: 'get',
-			url: 'api.php?op=create_script&name='+scrName,
-			success: function(res) {
-				if (res === 'OK') {
-					loadFilesList();
-				} else {
-					error(res);
+	var api = {
+		createNewScript: function(scrName) {
+			return $.ajax({
+				type: 'get',
+				url: 'api.php?op=create_script&name='+scrName,
+				success: function(res) {
+					if (res === 'OK') {
+						api.loadFilesList();
+					} else {
+						error(res);
+					}
+				},
+				error: function() {
+					error('Creating new script failed');
 				}
-			},
-			error: function() {
-				error('Creating new script failed');
+			});
+		},
+
+		// get the list of the files and update the list
+		loadFilesList: function() {
+			return $.ajax({
+				type: 'get',
+				url: 'api.php?op=list_scripts',
+				success: function(res) {
+					try {
+						var list = JSON.parse(res);
+					} catch(e) {
+						error('ERROR: ' + res);
+						return;
+					}
+
+					filesList.set(list);
+
+				},
+				error: function() {
+					error('error loading list of scripts');
+				}
+			});
+		},
+
+		loadScriptContent: function(scrName) {
+			return $.ajax({
+				type: 'get',
+				url: 'api.php?op=get_script&name='+scrName,
+				success: function(res) {
+					// update the editor
+					editor.setContent(res);
+					
+					// set the currently selected script name
+					filesList.selectedScriptName = scrName;
+					
+					// enable the save button
+					$('#save-file-content-btn').removeClass('disabled');
+					
+					notify('Script content loaded successfully');
+				},
+				error: function() {
+					error('Cannot load script ' + scrName);
+				}
+			});
+		},
+
+		renameScript: function(scrName, newScrName) {
+			return $.ajax({
+				type: 'get',
+				url: 'api.php?op=rename_script&name='+scrName+'&new_name='+newScrName,
+				success: function(res) {
+					if (res === 'OK') {
+						// update the list
+						api.loadFilesList();
+						
+						notify('Renamed successfully');
+						
+						// load the new content to the editor
+						api.loadScriptContent(newScrName);
+					} else {
+						error(res);
+					}
+				},
+				error: function() {
+					error('Cannot load script ' + scrName);
+				}
+			});
+		},
+	
+		deleteScript: function(scrName) {
+			return $.ajax({
+				type: 'get',
+				url: 'api.php?op=delete_script&name='+scrName,
+				success: function(res) {
+					if (res === 'OK') {
+						// update the list
+						api.loadFilesList();
+						
+						notify('Deleted successfully');
+					} else {
+						error(res);
+					}
+				},
+				error: function() {
+					error('Cannot load script ' + scrName);
+				}
+			});
+		},
+
+		saveScriptContent: function() {
+			if (!filesList.selectedScriptName) {
+				error('No script is selected');
+				return;
 			}
-		});
+			
+			$('#save-file-content-btn').addClass('disabled');
+			
+			return $.ajax({
+				type: 'post',
+				url: 'api.php?op=update_script&name='+filesList.selectedScriptName,
+				data: {
+					content: editor.getContent()
+				},
+				success: function(res) {
+					if (res === 'OK') {
+						notify('Saved successfully');
+						$('#save-file-content-btn').removeClass('disabled');
+					} else {
+						error(res);
+					}
+				},
+				error: function() {
+					error('Cannot load script ' + scrName);
+				}
+			});
+		}
 	};
 
-	var addNewScriptToList = function(scrName) {
-		var newLi = $(
-			'<li class="collection-item">' +
-			'	<a href="scripts/' + scrName + '" target="_blank" title="Execute the script in a separate tab/window">' + scrName + '</a>' + 
-			'	<div class="secondary-content">' + 
-			'		<a href="javascript:;" script-name="' + scrName + '" class="delete-script" title="Delete the script">' + 
-			'			<i class="material-icons">delete</i>' + 
-			'		</a>' + 
-			'		<a href="javascript:;" script-name="' + scrName + '" class="rename-script" title="Rename the script">' + 
-			'			<i class="material-icons">mode_edit</i>' + 
-			'		</a>' + 
-			'		<a href="javascript:;" script-name="' + scrName + '" class="load-source-link" title="Load the source code">' + 
-			'			<i class="material-icons">code</i>' + 
-			'		</a>' + 
-			'	</div>' + 
-			'</li>'
-		).insertAfter($('ul#files-list li.collection-header'));
-	}
+	var filesList = {
+		// current selected script name on the files list on the left
+		selectedScriptName: null,
 
-	var loadFilesList = function() {
-		return $.ajax({
-			type: 'get',
-			url: 'api.php?op=list_scripts',
-			success: function(res) {
-				try {
-					var filesList = JSON.parse(res);
-				} catch(e) {
-					error('ERROR: ' + res);
-					return;
-				}
+		// clear the list
+		clear: function() {
+			$('ul#files-list li.collection-header').nextAll().remove();
+		},
+	
+		// add new file to the list 
+		add: function(scrName) {
+			var newLi = $(
+				'<li class="collection-item">' +
+				'	<a href="scripts/' + scrName + '" target="_blank" title="Execute the script in a separate tab/window">' + scrName + '</a>' + 
+				'	<div class="secondary-content">' + 
+				'		<a href="javascript:;" script-name="' + scrName + '" class="delete-script" title="Delete the script">' + 
+				'			<i class="material-icons">delete</i>' + 
+				'		</a>' + 
+				'		<a href="javascript:;" script-name="' + scrName + '" class="rename-script" title="Rename the script">' + 
+				'			<i class="material-icons">mode_edit</i>' + 
+				'		</a>' + 
+				'		<a href="javascript:;" script-name="' + scrName + '" class="load-source-link" title="Load the source code">' + 
+				'			<i class="material-icons">code</i>' + 
+				'		</a>' + 
+				'	</div>' + 
+				'</li>'
+			).insertAfter($('ul#files-list li.collection-header'));
+		},
 
-				$('ul#files-list li.collection-header').nextAll().remove();
+		// populate list
+		set: function(list) {
+			// clear the list
+			filesList.clear();
 
-				// fill the list with the existing scripts
-				if (filesList.length > 0) {
-					filesList.forEach(function(scrName) {
-						addNewScriptToList(scrName);
-					});
-				} else {
-					var newLi = $('<li>')
-						.addClass('collection-item')
-						.text('No scripts :(');
+			// fill the list with the existing scripts
+			if (list.length > 0) {
+				list.forEach(function(scrName) {
+					filesList.add(scrName);
+				});
+			} else {
+				var newLi = $('<li>')
+					.addClass('collection-item')
+					.text('No scripts :(');
 
-					$('#files-list').append(newLi);
-				}
-			},
-			error: function() {
-				error('error loading list of scripts');
+				$('#files-list').append(newLi);
 			}
-		});
+		}
+
 	};
 
-	var codeMirror = {
+	// the CodeMirror object
+	var editor = {
 		obj: null,
 		init: function() {
-			if (!codeMirror.obj) {
+			if (!editor.obj) {
 				// initialize the codemirror object
-				codeMirror.obj = CodeMirror($('#file-content').get(0), {
+				editor.obj = CodeMirror($('#file-content').get(0), {
 					lineNumbers: true,
 					mode: "htmlmixed",
 					extraKeys: {
-						'Ctrl-S': saveScriptContent
+						'Ctrl-S': api.saveScriptContent
 					}
-					//saveFunction: saveScriptContent
 				});
-				codeMirror.obj.setSize('100%', '100%');
+				editor.obj.setSize('100%', '100%');
 			}
 		},
 		setContent: function(content) {
-			codeMirror.init();
-			codeMirror.obj.setValue(content);
+			editor.init();
+			editor.obj.setValue(content);
 		}, 
 		getContent: function() {
-			codeMirror.init();
-			return codeMirror.obj.getValue();
+			editor.init();
+			return editor.obj.getValue();
 		}
 	};
-
-	var loadScriptContent = function(scrName) {
-		return $.ajax({
-			type: 'get',
-			url: 'api.php?op=get_script&name='+scrName,
-			success: function(res) {
-				// update the editor
-				codeMirror.setContent(res);
-				
-				// set the currently selected script name
-				selectedScriptName = scrName;
-				
-				// enable the save button
-				$('#save-file-content-btn').removeClass('disabled');
-				
-				notify('Script content loaded successfully');
-			},
-			error: function() {
-				error('Cannot load script ' + scrName);
-			}
-		});
-	}
 	
-	var saveScriptContent = function() {
-		
-		if (!selectedScriptName) {
-			error('No script is selected');
-			return;
-		}
-		
-		$('#save-file-content-btn').addClass('disabled');
-		
-		return $.ajax({
-			type: 'post',
-			url: 'api.php?op=update_script&name='+selectedScriptName,
-			data: {
-				content: codeMirror.getContent()
-			},
-			success: function(res) {
-				if (res === 'OK') {
-					notify('Saved successfully');
-					$('#save-file-content-btn').removeClass('disabled');
-				} else {
-					error(res);
-				}
-			},
-			error: function() {
-				error('Cannot load script ' + scrName);
-			}
-		});
-	}
 	
-	var renameScript = function(scrName, newScrName) {
-		
-		return $.ajax({
-			type: 'get',
-			url: 'api.php?op=rename_script&name='+scrName+'&new_name='+newScrName,
-			success: function(res) {
-				if (res === 'OK') {
-					// update the list
-					loadFilesList();
-					
-					notify('Renamed successfully');
-					
-					// load the new content to the editor
-					loadScriptContent(newScrName);
-				} else {
-					error(res);
-				}
-			},
-			error: function() {
-				error('Cannot load script ' + scrName);
-			}
-		});
-	}
-	
-	var deleteScript = function(scrName) {
-		
-		return $.ajax({
-			type: 'get',
-			url: 'api.php?op=delete_script&name='+scrName,
-			success: function(res) {
-				if (res === 'OK') {
-					// update the list
-					loadFilesList();
-					
-					notify('Deleted successfully');
-				} else {
-					error(res);
-				}
-			},
-			error: function() {
-				error('Cannot load script ' + scrName);
-			}
-		});
-	}
 	
 	var fixHeights = function() {
 		var height = parseInt($(window).height()) - 200;
@@ -210,7 +228,7 @@
 	$(function() {
 
 		// get list of scripts from the server
-		loadFilesList();
+		api.loadFilesList();
 
 		// add new script callback
 		$('#new-script-name-form').on('submit', function(evt) {
@@ -219,7 +237,7 @@
 			
 			var val = $('#new-script-name').val().trim();
 			if (val) {
-				createNewScript(val).always(function() {
+				api.createNewScript(val).always(function() {
 					// close the modal
 					$('#add-new-script-modal').closeModal();
 				});
@@ -235,7 +253,7 @@
 
 				var scriptName = $(this).attr('script-name');
 				if (scriptName) {
-					loadScriptContent(scriptName);
+					api.loadScriptContent(scriptName);
 				}
 			})
 			.on('click', '.rename-script', function() {
@@ -247,7 +265,7 @@
 					return;
 				}
 				
-				renameScript(scriptName, newName);
+				api.renameScript(scriptName, newName);
 				
 			})
 			.on('click', '.delete-script', function() {
@@ -258,12 +276,12 @@
 				// get the script name to delete
 				var scriptName = $(this).attr('script-name');
 				
-				deleteScript(scriptName);
+				api.deleteScript(scriptName);
 				
 			});
 		
 		
-		$('#save-file-content-btn').on('click', saveScriptContent);
+		$('#save-file-content-btn').on('click', api.saveScriptContent);
 		
 		// initialize the model object
 		$('.modal-trigger').leanModal();
